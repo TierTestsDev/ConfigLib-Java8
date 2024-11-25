c97b1031918ca51839267cff888295e382a5bfdc
Justin Edgington <63556914+PillageDev@users.noreply.github.com>
2024-11-25 17:51:22 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,3 @@ include("ConfigLib-Core")
 findProject(":ConfigLib-Core")?.name = "configlib-core"
 include("ConfigLib-Bukkit")
 findProject(":ConfigLib-Bukkit")?.name = "configlib-bukkit"
-include("ConfigLib-Bungee")
-findProject(":ConfigLib-Bungee")?.name = "configlib-bungee"
-
